44bd28b1b99ec951ab053b8eb6238a914de673bf
Mike Birdgeneau <mike.birdgeneau@gmail.com>
2015-03-21 10:03:40 -0600

    Added comments to frontend file.

---
+++ rpiBrewControl/frontend.py
@@ -0,0 +1,8 @@
+# Nothing here yet!! :)
+
+# Planned Functionality:
+#- Summary Table of Sensors / Status
+#- View Trends vs. Setpoints
+#- Change Setpoints
+#- Change PID settings
+#- Export Data / Charts 
