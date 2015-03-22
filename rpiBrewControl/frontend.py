3a7412daf955d8cc5f8384c8d47d154ad78addf0
Mike Birdgeneau <mike.birdgeneau@gmail.com>
2015-03-22 13:34:53 -0600

    Updated sample frontend script.

--- rpiBrewControl/frontend.py
+++ rpiBrewControl/frontend.py
@@ -1,34 +1,21 @@
 # Nothing here yet!! :)
 
 from flask import Flask
-import numpy as np
-import cStringIO
-import matplotlib.pyplot as plt
+#import numpy as np
+#import cStringIO
+#import matplotlib.pyplot as plt
 import sqlite3
 from dbFunctions import *
 
 
 app = Flask(__name__)
 
-@app.route('/plot')
-def build_plot():
-
-  # Generate the plot
-  x = np.linspace(0, 10)
-  line, = plt.plot(x, np.sin(x))
-
-  f = cStringIO.StringIO()
-  plt.savefig(f, format='png')
-
-  # Serve up the data
-  header = {'Content-type': 'image/png'}
-  f.seek(0)
-  data = f.read()
-
-  return data, 200, header
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-  app.run()
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
 # Planned Functionality:
 #- Summary Table of Sensors / Status
