8f33aeeaf28552aba2265e9983f01db06e547320
Mike Birdgeneau <mike.birdgeneau@gmail.com>
2015-03-23 07:07:55 -0600

    Updates to UI with JS auto-refresh.

--- rpiBrewControl/www/cgi/brewmaster.py
+++ rpiBrewControl/www/cgi/brewmaster.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
+# Load modules
+import time, os
+import yaml
+import sqlite3
+import datetime
 import cgitb; cgitb.enable()
 import cgi
 import os
 import sys
 
-os.chdir("../../")
+os.chdir("../")
 sys.path.append(os.getcwd())
 
 print "Content-Type: text/html\n"
@@ -15,18 +20,21 @@ from dbFunctions import *
 from configFunctions import *
 
 global config
-config = yaml.load(file("config.yml"))
+config = yaml.load(file(os.getcwd()+"/"+"config.yml"))
 
 # Load Configuration into Sensor Classes
 sensors = loadSensorConfig(config)
 
 # Create database connection
-conn = sqlite3.connect("../../"+config['dbFile'])
+conn = sqlite3.connect(os.getcwd()+"/"+config['dbFile'])
 
 input_data  = cgi.FieldStorage()
 if input_data['func'].value == "equipSummary":
-	print "Equipment Summary: "
-	for sensor in sensors:
-		print sensor.id +": " + sensor.name
+    print "<table class='table table-striped table-responsive'><tr><th>Name</th><th>ID</th><th>Temp (C)</th><th>Setpoint (C)</th><th>Status</th><th>Duty</th></tr>"
+    
+    for sensor in sensors:
+        print "<tr><td>"+sensor.name+"</td><td>"+sensor.id+"</td><td>TODO!</td><td>TODO</td><td>TODO</td><td>TODO</td></tr>"
+    print "</table>"
+    
 else:
-	print "hello"
+	print "TODO: Temperature Graphs"
